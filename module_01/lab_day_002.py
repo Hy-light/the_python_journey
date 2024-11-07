d5f99dcd95aed02691727d829db358e3d960512a
Prince Chime <chimeprince92@gmail.com>
2024-11-07 14:51:04 +0000

    Updated files with progress work

---
+++ module_01/lab_day_002.py
@@ -0,0 +1,54 @@
+# Lab implementation in Vs code
+
+# Try your first Python output
+print('Hello, Python!')
+
+# Check the Python Version
+import sys
+print(sys.version)
+
+# Practice on writing comments
+print('Hello, Prince learning python !') # This line prints a string
+# print('Hi')
+
+# ------------ Data Types in Python: Numbers (integers and floats) and Strings -------------
+# Type of 12
+print(type(12)) # Int
+
+# Type of 2.14
+print(type(2.14)) # float
+
+# Type of "Hello, Python 101!"
+print(type("Hello, Python 101!")) #Str
+
+# Type of True
+print(type(True)) # Boolean
+
+# Type of False
+print(type(False)) # Boolean
+
+# --------- Converting from one type to another ( Casting )----------
+# Convert 2 to a float
+print(float(2))
+
+# Convert a string into an integer
+print(type(int('1')))
+
+# Convert the string "1.2" into a float
+print(type(float('1.2')))
+
+# Convert an integer to a string
+print(type(str(1)))
+
+# Convert a float to a string
+print(type(str(1.2)))
+
+# Convert True to int
+print(int(True)) # returns 1
+
+# Convert 1 to boolean
+print(bool(1)) # returns a True
+
+# Convert 0 to boolean
+print(bool(0))
+
