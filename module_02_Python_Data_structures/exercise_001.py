c92fe97652ab09eb7a6140da32cb0b4d06c8964c
Prince Chime <chimeprince92@gmail.com>
2024-11-12 17:57:05 +0000

    completed lab for List

--- module_02_Python_Data_structures/exercise_001.py
+++ module_02_Python_Data_structures/exercise_001.py
@@ -26,4 +26,23 @@ print(sorted_Tuple)
 
 
 
+# -------------------- List ----------------------------
+# Create a list a_list, with the following elements 1, hello, [1,2,3] and True.
+a_list = [1, 'hello', [1,2,3], True]
+print(a_list)
+
+# Find the value stored at index 1 of a_list.
+print(a_list[1])
+
+# Retrieve the elements stored at index 1, 2 and 3 of a_list.
+print(a_list[1:4])
+
+# Concatenate the following lists A = [1, 'a'] and B = [2, 1, 'd']:
+A = [1, 'a'] 
+B = [2, 1, 'd']
+C = A + B
+print(C)
+
+
+
 
