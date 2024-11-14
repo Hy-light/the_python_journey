9734ed9b95f693b222301655289e8874ce1e8f33
Prince Chime <chimeprince92@gmail.com>
2024-11-14 17:16:42 +0000

    started module three

---
+++ module_03_python_programming_fundamentals/lap_001.py
@@ -0,0 +1,26 @@
+# Comparison Operations
+# Find the value of i that produces a True:
+i = 1
+i != 0
+if i == 1:
+    print(True)
+else:
+    print(False)
+
+# Branching
+
+# Find the value of x that prints the statement "this is a":
+x = 'a'
+if(x=='a'):
+    print("this is a")
+else:
+    print("this is  not a")
+
+# Logic Operators
+# Find the value of y that produces a True statement:
+y = 0 
+x = 1
+z = x > 0 and y < 10
+print(z)
+
+
