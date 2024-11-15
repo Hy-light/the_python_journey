3a008a77482d6f0fcaf825a98a2f0b7ecd7f543e
Prince Chime <chimeprince92@gmail.com>
2024-11-15 19:18:12 +0000

    Complete lab on loops

--- module_03_python_programming_fundamentals/exercise_001.py
+++ module_03_python_programming_fundamentals/exercise_001.py
@@ -28,3 +28,109 @@ elif (age >= 15 ) and (age <= 17 ):
     print("You will have burger for breakfast")
 else:
     print("You are not covered")
+
+
+
+# ----------------------- loops -----------------------
+# Use loops to print out the elements in the list A:
+A = [3,4,5]
+for item in A:
+    print('...printing', item)
+
+
+# While Loops
+# Find the value of  x that will print out the sequence  1,2,..,10 :
+x = 11
+y = 1
+while(y != x):
+    print(y)
+    y += 1
+
+
+# Loops exercises
+# Write a for loop the prints out all the element between -5 and 5 using the range function.
+
+for i in range(-5, 5):
+    print(i)
+
+# Print the elements of the following list: Genres=[ 'rock', 'R&B', 'Soundtrack', 'R&B', 'soul', 'pop'] Make sure you follow Python conventions.
+Genres = [ 'rock', 'R&B', 'Soundtrack', 'R&B', 'soul', 'pop']
+for genre in Genres:
+    print(genre)
+
+
+# Write a for loop that prints out the following list:
+squares = ['red', 'yellow', 'green', 'purple', 'blue']
+for square in squares:
+    print(square)
+
+
+# Write a while loop to display the values of the Rating of an album playlist stored in the list PlayListRatings. 
+# If the score is less than 6, exit the loop. The list PlayListRatings is given by: PlayListRatings = [10, 9.5, 10, 8, 7.5, 5, 10, 10]
+
+PlayListRatings = [10, 9.5, 10, 8, 7.5, 5, 10, 10]
+i = 0
+score = PlayListRatings[i]
+
+while (i < len(PlayListRatings) and score >= 6):
+    print(score)
+    i += 1
+    score = PlayListRatings[i]
+
+
+# Write a while loop to copy the strings 'orange' of the list squares_1 to the list new_squares_1. 
+# Stop and exit the loop if the value on the list is not 'orange':
+
+squares_1 = ['orange', 'orange', 'purple', 'blue ', 'orange']
+new_squares_1 = []
+i = 0
+
+while (i < len(squares_1) and squares_1[i] == 'orange'):
+    new_squares_1.append(squares_1[i])
+    i += 1
+    print(new_squares_1)
+
+'''
+Some real-life problems!¶
+Your little brother has just learned multiplication tables in school. Today he has learned tables of 6 and 7. 
+Help him memorise both the tables by printing them using for loop.
+'''
+# for loop for table 6
+for i in range(13):
+    xpl = i * 6
+    print('6 *', i, 'is = ', xpl )
+
+for i in range(13):
+    xpl = i * 7
+    print('7 *', i, 'is = ', xpl )
+
+
+'''
+The following is a list of animals in a National Zoo. Animals = ["lion", "giraffe", "gorilla", "parrots", "crocodile","deer", "swan"]
+Your brother needs to write an essay on the animals whose names are made of 7 letters. 
+Help him find those animals through a while loop and create a separate list of such animals.
+'''
+
+'''
+Animals = ["lion", "giraffe", "gorilla", "parrots", "crocodile","deer", "swan"]
+Animal_7_letters = []
+i = 0
+
+while (i < len(Animals) and len(Animals[i]) == 7):
+    Animal_7_letters.append(Animals[i])
+    i += 1
+    print('Animals made up of seven are: ',Animal_7_letters)
+'''
+
+Animals = ["lion", "giraffe", "gorilla", "parrots", "crocodile","deer", "swan"]
+Animal_7_letters = []
+i = 0
+while (i < len(Animals)):
+    if len(Animals[i]) == 7:
+        Animal_7_letters.append(Animals[i])
+        print(f"Adding value for {Animals[i]}, to the new list")
+    else:
+        print(f"Skipping {Animals[i]}, as length is not 7")
+
+    i += 1
+print(f"Animals made up of seven letter are: {Animal_7_letters}")
--- module_03_python_programming_fundamentals/lap_001.py
+++ module_03_python_programming_fundamentals/lap_001.py
@@ -23,5 +23,21 @@ x = 1
 z = x > 0 and y < 10
 print(z)
 
-# Lab - Conditions and Branching
+# If statement example
 
+age = 19
+# age = 18
+#expression that can be true or false
+if age > 18:
+    
+    #within an indent, we have the expression that is run if the condition is true
+    print("Hey, you can enter" )
+
+#The statements after the if statement will run regardless if the condition is true or false 
+print("Move on!")
+
+# Condition statement example
+
+album_year = 1983
+if not (album_year == 1984):
+    print ("Album year is not 1984")
